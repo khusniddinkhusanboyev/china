ced7d8e3290c3219671c3435684a5d435952fcd9
khusniddin007 <xxusanboyev323@gmail.com>
2023-08-02 23:21:19 +0500

    first

---
+++ src/main/java/uz/sale/car/china/ChinaApplication.java
@@ -0,0 +1,13 @@
+package uz.sale.car.china;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinaApplication.class, args);
+	}
+
+}
